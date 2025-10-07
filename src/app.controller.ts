efc7412b8895aa604a57d8a13edf96cc08dbce7b
Sean Huang <castle2668@gmail.com>
2025-10-07 10:02:51 +0800

    新增 MessageModule 及相關提供者，更新 AppModule 以注入作者資訊和自定義訊息，並擴展 AppController 以使用這些依賴。

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,24 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Inject, Optional, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { of } from 'rxjs';
 import { Response } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('AUTHOR') private readonly author: string,
+    @Inject('AUTHOR_MESSAGE') private readonly authorMessage: string,
+    @Inject('ALIAS_APP_SERVICE') private readonly aliasAppService: AppService,
+    @Inject('CUSTOM_MESSAGE') private readonly customMessage: string,
+    @Optional() @Inject('NO_EXIST') private readonly noExist: unknown,
+  ) {
+    console.log(this.appService === this.aliasAppService); // true
+    console.log(this.author); // Sean
+    console.log(this.authorMessage); // Sean: Hello World!
+    console.log(this.customMessage); // message!
+    console.log(this.noExist); // undefined
+  }
 
   @Get()
   getHello(): string {
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,32 @@ import { TodoController } from './features/todo/todo.controller';
 import { TodoModule } from './features/todo/todo.module';
 import { CopyTodoModule } from './features/copy-todo/copy-todo.module';
 import { CommonModule } from './modules/common/common.module';
+import { MessageModule } from './modules/message/message.module';
 
 @Module({
-  imports: [TodoModule, CopyTodoModule, CommonModule],
+  imports: [TodoModule, CopyTodoModule, CommonModule, MessageModule],
   controllers: [AppController, TodoController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: AppService,
+      useClass: AppService,
+    },
+    {
+      provide: 'AUTHOR',
+      useValue: 'Sean',
+    },
+    {
+      provide: 'AUTHOR_MESSAGE',
+      inject: [AppService],
+      useFactory: (appService: AppService) => {
+        const hello = appService.getHello();
+        return `Sean: ${hello}`;
+      },
+    },
+    {
+      provide: 'ALIAS_APP_SERVICE',
+      useExisting: AppService,
+    },
+  ],
 })
 export class AppModule {}
