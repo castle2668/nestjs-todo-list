e8beb070b835c454198d91fd195bf83a27646108
Sean Huang <castle2668@gmail.com>
2025-10-07 10:57:16 +0800

    新增 HttpFilter 以處理 HTTP 異常，並更新 AppModule 以注入該過濾器。

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,48 +1,13 @@
-import { Controller, Get, Inject, Optional, Res } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
-import { of } from 'rxjs';
-import { Response } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    @Inject('AUTHOR') private readonly author: string,
-    @Inject('AUTHOR_MESSAGE') private readonly authorMessage: string,
-    @Inject('ALIAS_APP_SERVICE') private readonly aliasAppService: AppService,
-    @Inject('CUSTOM_MESSAGE') private readonly customMessage: string,
-    @Optional() @Inject('NO_EXIST') private readonly noExist: unknown,
-  ) {
-    console.log(this.appService === this.aliasAppService); // true
-    console.log(this.author); // Sean
-    console.log(this.authorMessage); // Sean: Hello World!
-    console.log(this.customMessage); // message!
-    console.log(this.noExist); // undefined
-  }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    throw new HttpException('發生錯誤!', HttpStatus.BAD_REQUEST);
     return this.appService.getHello();
-  }
-
-  @Get('async')
-  testPromise() {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve([]);
-      }, 1000);
-    });
-  }
-
-  @Get('rxjs')
-  testRxjs() {
-    return of([]);
-  }
-
-  @Get('libraryMode')
-  testLibraryMode(@Res() res: Response) {
-    res.status(200).json({
-      message: 'Hello World',
-    });
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,35 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TodoController } from './features/todo/todo.controller';
 import { TodoModule } from './features/todo/todo.module';
 import { CopyTodoModule } from './features/copy-todo/copy-todo.module';
 import { CommonModule } from './modules/common/common.module';
 import { MessageModule } from './modules/message/message.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpFilter } from './filters/http/http.filter';
+import { AppService } from './app.service';
 
 @Module({
   imports: [TodoModule, CopyTodoModule, CommonModule, MessageModule],
   controllers: [AppController, TodoController],
   providers: [
+    AppService,
     {
-      provide: AppService,
-      useClass: AppService,
-    },
-    {
-      provide: 'AUTHOR',
-      useValue: 'Sean',
-    },
-    {
-      provide: 'AUTHOR_MESSAGE',
-      inject: [AppService],
-      useFactory: (appService: AppService) => {
-        const hello = appService.getHello();
-        return `Sean: ${hello}`;
-      },
-    },
-    {
-      provide: 'ALIAS_APP_SERVICE',
-      useExisting: AppService,
+      provide: APP_FILTER,
+      useClass: HttpFilter,
     },
   ],
 })
