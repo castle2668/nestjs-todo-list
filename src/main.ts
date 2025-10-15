d73c575ddc22facd6bd8b281c68922ed1f6e7cfe
Sean Huang <castle2668@gmail.com>
2025-10-15 10:18:36 +0800

    Feat(main): Update Swagger configuration and remove CORS settings
    1. Change Swagger title and description to reflect the Todo List API.
    2. Add Bearer authentication support in Swagger configuration.
    3. Remove CORS settings for the application.

--- src/main.ts
+++ src/main.ts
@@ -10,18 +10,16 @@ import { INestApplication } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
-  app.enableCors({
-    origin: 'http://mock.icp-si.com:8080',
-  });
   await app.listen(3000);
 }
 
 function setupSwagger(app: INestApplication) {
   const builder = new DocumentBuilder();
   const config = builder
-    .setTitle('NestJS Practice')
-    .setDescription('NestJS Practice API')
+    .setTitle('NestJS Todo List')
+    .setDescription('NestJS Todo List API')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const options: SwaggerCustomOptions = { explorer: true };
   const document = SwaggerModule.createDocument(app, config);
